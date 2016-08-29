9a9e2ff1b479d53a62f2b0835415416e4d322ed6
Bradley Guthrie <guppy96@gmail.com>
2016-08-29 16:37:10 -0500

    15 column change

--- SQL_Learing_Thru_Doing_Question_15.sql
+++ SQL_Learing_Thru_Doing_Question_15.sql
@@ -3,7 +3,7 @@
 
 use Chinook;
 
-SELECT Playlist.PlaylistId, Playlist.Name AS 'PlayList Catergory', COUNT(PlaylistTrack.TrackId) AS 'PlayList Total'
+SELECT Playlist.PlaylistId, Playlist.Name AS 'PlayList Catergory', COUNT(PlaylistTrack.TrackId) AS 'Total Number of Tracks'
 FROM Playlist 
 JOIN PlaylistTrack ON Playlist.PlaylistId = PlaylistTrack.PlaylistId 
 GROUP BY PlayList.PlaylistId,Playlist.Name;
