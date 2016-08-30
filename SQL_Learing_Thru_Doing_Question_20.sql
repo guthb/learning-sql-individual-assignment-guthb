dcdd47664e80cd42c68e651c12b1381350e0e7a4
Bradley Guthrie <guppy96@gmail.com>
2016-08-30 17:51:05 -0500

    20 complete

---
+++ SQL_Learing_Thru_Doing_Question_20.sql
@@ -0,0 +1,11 @@
+--Which sales agent made the most in sales in 2010?
+
+use Chinook;
+
+SELECT TOP 1 Employee.FirstName + ' ' + Employee.LastName AS 'Sales Agent', SUM(Invoice.Total) AS 'Max TotalInvoice' 
+FROM Invoice 
+INNER JOIN Customer ON Invoice.CustomerID = Customer.CustomerID
+INNER JOIN Employee ON Customer.SupportRepID = Employee.EmployeeID
+WHERE Invoice.InvoiceDate BETWEEN '2010-01-01' and '2010-12-31'
+GROUP BY  Employee.FirstName,Employee.LastName
+ORDER BY 'Max TotalInvoice' DESC;
