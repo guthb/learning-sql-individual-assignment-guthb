122a410e7b7d8b03eaa12d1e3d0b644e1dda20f5
Bradley Guthrie <guppy96@gmail.com>
2016-08-30 18:31:52 -0500

    22 complete

---
+++ SQL_Learing_Thru_Doing_Question_22.sql
@@ -0,0 +1,8 @@
+--Provide a query that shows the # of customers assigned to each sales agent.
+
+use Chinook;
+
+SELECT Employee.FirstName+' '+Employee.LastName As 'Sales Agent', Count(Customer.CustomerId) as 'Customers Total' 
+FROM Employee 
+JOIN Customer ON Employee.EmployeeId = Customer.SupportRepId 
+GROUP BY Employee.EmployeeId, Employee.FirstName, Employee.LastName;
