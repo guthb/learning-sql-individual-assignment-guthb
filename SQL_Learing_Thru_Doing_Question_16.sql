e224e36ca8607718f330d00542dfdc1207c2a49d
Bradley Guthrie <guppy96@gmail.com>
2016-08-30 19:44:06 -0500

    16 readability

--- SQL_Learing_Thru_Doing_Question_16.sql
+++ SQL_Learing_Thru_Doing_Question_16.sql
@@ -4,7 +4,9 @@
 use Chinook;
 
 
-SELECT Track.Name AS 'Track Name', Album.Title AS 'Album Name', MediaType.Name AS 'Media Type', Genre.Name AS 'Genre' FROM Track
+SELECT Track.Name AS 'Track Name', Album.Title AS 'Album Name', 
+	MediaType.Name AS 'Media Type', Genre.Name AS 'Genre' 
+FROM Track
 JOIN Album ON Track.AlbumId = Album.AlbumId
 JOIN MediaType ON Track.MediaTypeId = MediaType.MediaTypeId
 JOIN Genre ON Track.GenreId = Genre.GenreId;
