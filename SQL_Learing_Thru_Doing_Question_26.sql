92cf66f8ff4da35a8889ad92ab6a92a0784e0156
Bradley Guthrie <guppy96@gmail.com>
2016-08-30 19:26:24 -0500

    26 complete

---
+++ SQL_Learing_Thru_Doing_Question_26.sql
@@ -0,0 +1,12 @@
+--Provide a query that shows the top 3 best selling artists
+
+use Chinook;
+
+SELECT TOP 3 Artist.Name As 'Artist', SUM(InvoiceLine.Quantity) AS 'Total Track Sales'
+FROM Track
+JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
+JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.InvoiceId
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist ON Album.ArtistId = Artist.ArtistId
+GROUP BY Artist.ArtistId, Artist.Name
+ORDER BY 'Total Track Sales' DESC;
